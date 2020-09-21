4d85561f3d50feacdf692e24773256fda4a2b3bf
Anton <popovanton567@gmail.com>
2020-09-21 19:01:45 +0200

    Add Rust language

--- update_readme.py
+++ update_readme.py
@@ -3,21 +3,23 @@ Script updates `README.md` with respect to files at ./easy and ./medium folders.
 """
 import os
 
-easy = sorted(os.listdir("./easy"))
-easy = [x.split("_")[0] for x in easy]
-
-medium = sorted(os.listdir("./medium"))
-medium = [x.split("_")[0] for x in medium]
 
 with open("README.md", 'w') as readme:
-    readme.write("# LeetCode\n")
-    readme.write("### Easy\n")
-    easy_solved = ""
-    for el in easy:
-        easy_solved += "{}, ".format(el)
-    readme.write(easy_solved[:-2] + "\n")
-    readme.write("### Medium\n")
-    medium_solved = ""
-    for el in medium:
-        medium_solved += "{}, ".format(el)
-    readme.write(medium_solved[:-2] + '\n')
+    readme.write("# LeetCode\nDeliberate practice in coding.\n")
+    langs = [l for l in os.listdir() if os.path.isdir(l) and l[0] != '.']
+    for lang in langs:
+        readme.write("## {}\n".format(lang))
+        readme.write("### Easy\n")
+        easy = sorted(os.listdir("./{}/easy".format(lang)))
+        easy = [x.split("_")[0] for x in easy]
+        easy_solved = ""
+        for el in easy:
+            easy_solved += "{}, ".format(el)
+        readme.write(easy_solved[:-2] + "\n")
+        readme.write("### Medium\n")
+        medium = sorted(os.listdir("./{}/medium".format(lang)))
+        medium = [x.split("_")[0] for x in medium]
+        medium_solved = ""
+        for el in medium:
+            medium_solved += "{}, ".format(el)
+        readme.write(medium_solved[:-2] + '\n')
