d3ab7769197729d5fedc6b48dc1511fe4b566fb0
Anton <popovanton567@gmail.com>
2020-11-09 12:30:25 +0100

    1221 0 ms, 2.2 MB

---
+++ Rust/easy/1221_split_a_string_in_balanced_strings.rs
@@ -0,0 +1,20 @@
+// Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+// Given a balanced string s split it in the maximum amount of balanced strings.
+// Return the maximum amount of splitted balanced strings.
+impl Solution {
+    pub fn balanced_string_split(s: String) -> i32 {
+        let mut x: i32 = 0;
+        let mut split_count: i32 = 0;
+        for idx in 0..s.len() {
+            if s[idx] == 'R' {
+                x += 1;
+            } else {
+                x -= 1;
+            }
+            if x == 0 {
+                split_count += 1;
+            }
+        }
+        split_count
+    }
+}
