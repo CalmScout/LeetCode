0754bf987bdc03ad5e3f2623d872b69ac2f6b8fc
Anton Popov <popovanton567@gmail.com>
2021-01-25 13:09:04 +0100

    Readme update

--- update_readme.py
+++ update_readme.py
@@ -3,21 +3,21 @@ Script updates `README.md` with respect to files at ./easy and ./medium folders.
 """
 import os
 
-
-with open("README.md", 'w') as readme:
+curr_dir = os.path.dirname(__file__)
+with open(os.path.join(curr_dir, "README.md"), 'w') as readme:
     readme.write("# LeetCode\nDeliberate practice in coding.\n")
-    langs = [l for l in os.listdir() if os.path.isdir(l) and l[0] != '.']
+    langs = [l for l in os.listdir(curr_dir) if os.path.isdir(os.path.join(curr_dir, l)) and l[0] != '.']
     for lang in langs:
         readme.write("## {}\n".format(lang))
         readme.write("### Easy\n")
-        easy = sorted(os.listdir("./{}/easy".format(lang)))
+        easy = sorted(os.listdir(f"{curr_dir}/{lang}/easy"))
         easy = [x.split("_")[0] for x in easy]
         easy_solved = ""
         for el in easy:
             easy_solved += "{}, ".format(el)
         readme.write(easy_solved[:-2] + "\n")
         readme.write("### Medium\n")
-        medium = sorted(os.listdir("./{}/medium".format(lang)))
+        medium = sorted(os.listdir(f"{curr_dir}/{lang}/medium"))
         medium = [x.split("_")[0] for x in medium]
         medium_solved = ""
         for el in medium:
